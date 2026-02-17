92e139cb199e06496d7cac7f1e103252e96d86ec
gahan10 <gahandwivedi@gmail.com>
2026-02-17 12:53:11 +0530

    few improvements

--- register.py
+++ register.py
@@ -1,6 +1,4 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from flask_scss import Scss
-from flask_sqlalchemy import SQLAlchemy
 from teachers import users
 from werkzeug.security import generate_password_hash, check_password_hash
 from __init__ import db
